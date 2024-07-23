f97e4d6f7ef97437adcfc01141f7517c27f7937d
ez4foxy <foxy.tigrrr@mail.ru>
2024-07-23 18:31:36 +0300

    Add files via upload

--- module_3_2.py
+++ module_3_2.py
@@ -1,12 +1,12 @@
-def send_email(messadge, recipient, sender= "university.help@gmail.com"):
-    if "@" not in recipient or (recipient.endswith(".com")) or (recipient.endswith(".ru")) or (recipient.endswith(".net")):
-        print("Невозможно отправить письмо с адреса,", sender, "на адрес", recipient)
-    if sender == recipient:
+def send_email(message, recipient, *, sender="university.help@gmail.com"):
+    if "@" not in sender or not sender.endswith((".com", ".ru", ".net")) or "@" not in recipient or not recipient.endswith((".com", ".ru", ".net")):
+        print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}")
+    elif sender == recipient:
         print("Нельзя отправить письмо самому себе!")
-    if sender == "university.help@gmail.com":
-        print("Письмо успешно отправлено с адреса", sender, "на адрес", recipient)
+    elif sender == "university.help@gmail.com":
+        print(f"Письмо успешно отправлено с адреса {sender} на адрес {recipient}.")
     else:
-        print("НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса", sender, "на адрес", recipient)
+        print(f"НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}.")
 
 
 
